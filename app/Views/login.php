60d5980b41ecb52ea66f4a94c0bcd80a13195638
Ahmad Imam Mutaqin <imammutakin602@gmail.com>
2026-04-15 15:35:27 +0700

    tambah hidden/visible password

--- app/Views/login.php
+++ app/Views/login.php
@@ -4,7 +4,6 @@
 <div class="flex justify-center items-center min-h-[70vh]">
     <div class="bg-white rounded-2xl shadow-2xl p-10 w-full max-w-lg">
         <h2 class="text-center text-2xl font-bold text-[#1C4D8D] mb-8 tracking-wide">LOGIN</h2>
-
         <form action="<?= base_url('login') ?>" method="post" class="flex-flex-col gap-6">
             <div class="flex flex-col">
                 <label class="font-semibold text-[#1C4D8D] text-sm mb-2" for="username">
@@ -17,17 +16,27 @@
                 <label class="font-semibold text-[#1C4D8D] text-sm mb-2" for="password">
                     Password
                 </label>
-                <input name="password" class="border border-gray-200 rounded-lg p-3 focus:outline-none focus:ring-1 focus:ring-[#1C4D8D] bg-white shadow-sm placeholder-gray-400 mb-3" id="password" placeholder="Masukkan password" required type="password" />
+                
+                <div class="relative">
+                    <input name="password" id="password" type="password" required placeholder="Masukkan password"
+                        class="w-full border border-gray-200 rounded-lg p-3 pr-10 focus:outline-none focus:ring-1 focus:ring-[#1C4D8D] bg-white shadow-sm placeholder-gray-400">
+                    
+                    <button type="button" onclick="showHide('password', 'eye_password')" 
+                            class="absolute inset-y-0 right-3 flex items-center text-gray-400">
+                        <i id="eye_password" class="fa-solid fa-eye-slash"></i>
+                    </button>
+                </div>
             </div>
-
+             <?php if (session()->getFlashdata('error')) : ?>
+                <div class="mt-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-lg relative mb-2 flex items-center gap-3" role="alert">
+                    <i class="fa-solid fa-circle-exclamation"></i>
+                    <span class="block sm:inline text-sm"><?= session()->getFlashdata('error') ?></span>
+                </div>
+            <?php endif; ?>
             <button class="bg-[#1C4D8D] w-full text-white p-3 rounded-lg font-bold shadow-md hover:bg-[#3E679E] transition mt-4" type="submit">
                 Login
             </button>
         </form>
-            <div class="text-right mt-4 text-sm text-gray-600">
-                <a href="#" onclick="bukaModalPassword()" class="text-[#1C4D8D] font-semibold hover:underline">Ganti password</a>
-            </div>
     </div>
 </div>
-
 <?= $this->endSection() ?>
